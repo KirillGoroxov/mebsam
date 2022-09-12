18ccefa49b95f0a41e9a8282c1339ce0f400ed1d
KirillGoroxov <106907758+KirillGoroxov@users.noreply.github.com>
2022-09-12 15:56:12 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const footer = document.querySelector(".footer");
+
+const footerText =
+    `<div class="footer__row">
+    <div class="footer__element footer__element_1">
+        <a href="mailto:agor31@yandex.ru">E-MAIL: agor31@yandex.ru</a>
+    </div>
+    <div class="footer__element footer__element_2">
+        <a href="https://tinyurl.com/bduzd4kp">VIBER: 8(996)729-02-54</a>
+    </div>
+    <div class="footer__element footer__element_3">
+        <span>ПО ВСЕМ ВОПРОСАМ ОБРАЩАТЬСЯ<br>ПО ТЕЛЕФОНУ ИЛИ В ВАЙБЕР</span>
+    </div>
+</div>`;
+footer.innerHTML = footerText;
+
+
+
